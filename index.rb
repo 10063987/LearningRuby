f0dca2cc703c1c06f2c2df73f1ad550107557891
Luis Valencia <luisvalencia.rincon@hotmail.com>
2017-06-26 12:27:06 -0500

    get input topic added

--- index.rb
+++ index.rb
@@ -1,3 +1,6 @@
+
+# GET INPUT
+# print will not add a new line after printing a string
 print 'Enter Value: '
 
 first_num = gets.to_i
@@ -6,6 +9,10 @@ print 'Enter another Value: '
 
 second_num = gets.to_i
 
+# puts will add a new line after printing a string
 puts first_num.to_s
-puts second_num.to_s 
+puts second_num.to_s
+
+print first_num.to_s + ' + ' + second_num.to_s + ' = ' + 
+(first_num + second_num).to_s
 
